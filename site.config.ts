a35143502156a0a00380ea77b670c90ce1369dfc
Rowan.Zhu <zhuxy78039@outlook.com>
2026-02-13 23:29:40 +0800

    Update rootNotionPageId in site configuration

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '06d3696e5bb80fcbe2be4ed99903dce',
+  rootNotionPageId: '306d3696e5bb80fcbe2be4ed99903dce',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
